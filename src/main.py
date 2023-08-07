3361627934d7f934cf2e1b1bfde1669d286592b2
FixedOctocat <egorfilatov2002@yandex.ru>
2023-08-08 00:54:56 +0300

    New admin features

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Response,status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.responses import JSONResponse
 import requests
 
@@ -17,67 +17,52 @@ async def login(token: str):
 @app.get("/api/v1/challenge/{task_id}/submit")
 async def challenge_submit(task_id: str):
     """Submit task via task_id"""
-    task = requests.get(f"{letoctf_api}/challenge/{task_id}/submit").json()
-    return task
+    try:
+        task = requests.get(f"{letoctf_api}/challenge/{task_id}/submit").json()
+        return task
+    except:
+        raise HTTPException(status_code=400, detail="Failed to submit challenge")
 
 @app.get("/api/v1/event")
 async def event(date: str = "2023-06-30"):
     """Get all events from api by date"""
-    events = requests.get(f"{letoctf_api}/event?date={date}").json()
+    try:
+        events = requests.get(f"{letoctf_api}/event?date={date}").json()
 
-    for event in events['data']['content']:
-        event['start']=event['start'].split(" ")[1].split(':')[0]+':'+event['start'].split(" ")[1].split(':')[1]
-        event['end']=event['end'].split(" ")[1].split(':')[0]+':'+event['end'].split(" ")[1].split(':')[1]
+        for event in events['data']['content']:
+            event['start'] = event['start'].split(" ")[1].split(':')[0] + ':' + event['start'].split(" ")[1].split(':')[1]
+            event['end'] = event['end'].split(" ")[1].split(':')[0] + ':' + event['end'].split(" ")[1].split(':')[1]
 
-    return events
+        return events
+    except:
+        raise HTTPException(status_code=500, detail="Failed to fetch events")
 
 @app.get("/api/v1/challenge")
 async def challenge(userId: str):
     """Get challenges for user"""
-    challenges = requests.get(f"{letoctf_api}/challenge?userId={userId}").json()
-    return challenges
+    try:
+        challenges = requests.get(f"{letoctf_api}/challenge?userId={userId}").json()
+        return challenges
+    except:
+        raise HTTPException(status_code=500, detail="Failed to fetch challenges")
 
 @app.get("/api/v1/challenge/{task_id}")
 async def challenge_description(task_id: str):
     """Get task description by task_id"""
-    task = requests.get(f"{letoctf_api}/challenge/{task_id}").json()
-    return task
+    try:
+        task = requests.get(f"{letoctf_api}/challenge/{task_id}").json()
+        return task
+    except:
+        raise HTTPException(status_code=404, detail="Task not found")
 
 @app.get("/api/v1/score/profile/{user_id}")
 async def profile(user_id: str):
     """Get user score by his id"""
-    user_data = requests.get(f"{letoctf_api}/challenge/{user_id}").json()
-    return user_data
-
-
-@app.get("/api/v1/scoreboard/users")
-async def score_users():
-    """Return user scoreboard"""
-    users = requests.get(f"{letoctf_api}/user").json()['data']['content']
-    out = [{"name": user['name'], "data": []} for user in users if user['admin']==False]
-    users_id = [user['id'] for user in users if user['admin']==False]
-
-    for i in range(len(users_id)):
-        user_data = requests.get(f"{letoctf_api}/score/user/{users_id[i]}/history").json()['data']['items']['content']
-        data = [{'x': i['submitted'], 'y': i['challenge']['weight']} for i in user_data]
-        out[i]["data"] = data
-
-    return out
-
-
-@app.get("/api/v1/scoreboard/teams")
-async def score_teams():
-    """Return team scoreboard"""
-    teams = requests.get(f"{letoctf_api}/team").json()['data']['content']
-    out = [{"name": team['name'], "data": []} for team in teams if team['admin']==False]
-    teams_id = [team['id'] for team in teams if team['admin']==False]
-
-    for i in range(len(teams_id)):
-        team_data = requests.get(f"{letoctf_api}/score/team/{teams_id[i]}/history").json()['data']['items']['content']
-        data = [{'x': i['submitted'], 'y': i['challenge']['weight']} for i in team_data]
-        out[i]["data"] = data
-
-    return out
+    try:
+        user_data = requests.get(f"{letoctf_api}/challenge/{user_id}").json()
+        return user_data
+    except:
+        raise HTTPException(status_code=500, detail="Failed to fetch user profile")
 
 @app.get("/api/v1/user/token/{token}")
 async def admin_login(token):
@@ -105,7 +90,7 @@ async def admin_get_challenge(task_id: str):
 async def admin_create_challenge(challenge_data: dict):
     """Создание новой задачи администратором"""
     try:
-        response = requests.post(f"{letoctf_api}/admin/challenge", json=challenge_data)
+        response = requests.post(f"{letoctf_api}/challenge", json=challenge_data)
         response.raise_for_status()
         created_challenge = response.json()
         return created_challenge
@@ -116,7 +101,7 @@ async def admin_create_challenge(challenge_data: dict):
 async def admin_update_challenge(task_id: str, challenge_data: dict):
     """Обновление задачи администратором по идентификатору"""
     try:
-        response = requests.put(f"{letoctf_api}/admin/challenge/{task_id}", json=challenge_data)
+        response = requests.put(f"{letoctf_api}/challenge/{task_id}", json=challenge_data)
         response.raise_for_status()
         updated_challenge = response.json()
         return updated_challenge
@@ -127,8 +112,90 @@ async def admin_update_challenge(task_id: str, challenge_data: dict):
 async def admin_delete_challenge(task_id: str):
     """Удаление задачи администратором по идентификатору"""
     try:
-        response = requests.delete(f"{letoctf_api}/admin/challenge/{task_id}")
+        response = requests.delete(f"{letoctf_api}/challenge/{task_id}")
         response.raise_for_status()
         return {"message": "Challenge deleted successfully"}
     except requests.exceptions.HTTPError:
         raise HTTPException(status_code=400, detail="Failed to delete challenge")
+
+
+@app.get("/api/v1/scoreboard/users")
+async def score_users(response: Response):
+    """Return user scoreboard"""
+    try:
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        users = requests.get(f"{letoctf_api}/user").json()['data']['content']
+        out = [{"name": user['name'], "data": []} for user in users if user['admin'] == False]
+        users_id = [user['id'] for user in users if user['admin'] == False]
+
+        for i in range(len(users_id)):
+            user_data = requests.get(f"{letoctf_api}/score/user/{users_id[i]}/history").json()['data']['items']['content']
+            data = [{'x': i['submitted'], 'y': i['challenge']['weight']} for i in user_data]
+            out[i]["data"] = data
+
+        return out
+    except:
+        raise HTTPException(status_code=500, detail="Failed to fetch user scoreboard")
+
+@app.get("/api/v1/scoreboard/teams")
+async def score_teams():
+    """Return team scoreboard"""
+    try:
+        teams = requests.get(f"{letoctf_api}/team").json()['data']['content']
+        out = [{"name": team['name'], "data": []} for team in teams if team['admin'] == False]
+        teams_id = [team['id'] for team in teams if team['admin'] == False]
+
+        for i in range(len(teams_id)):
+            team_data = requests.get(f"{letoctf_api}/score/team/{teams_id[i]}/history").json()['data']['items']['content']
+            data = [{'x': i['submitted'], 'y': i['challenge']['weight']} for i in team_data]
+            out[i]["data"] = data
+
+        return out
+    except:
+        raise HTTPException(status_code=500, detail="Failed to fetch team scoreboard")
+
+@app.get("/api/v1/score/profile/{user_id}")
+async def get_user_score(user_id: str):
+    """Get user score by his id"""
+    try:
+        user_data = requests.get(f"{letoctf_api}/challenge/{user_id}").json()
+        return user_data
+    except:
+        # Обработка исключений при получении данных о пользователе
+        pass
+
+@app.get("/api/v1/scoreboard/users")
+async def get_user_scoreboard():
+    """Return user scoreboard"""
+    try:
+        users = requests.get(f"{letoctf_api}/user").json()['data']['content']
+        out = [{"name": user['name'], "data": []} for user in users if not user['admin']]
+        users_id = [user['id'] for user in users if not user['admin']]
+
+        for i in range(len(users_id)):
+            user_data = requests.get(f"{letoctf_api}/score/user/{users_id[i]}/history").json()['data']['items']['content']
+            data = [{'x': i['submitted'], 'y': i['challenge']['weight']} for i in user_data]
+            out[i]["data"] = data
+
+        return out
+    except:
+        # Обработка исключений при получении данных о пользователе
+        pass
+
+@app.get("/api/v1/scoreboard/teams")
+async def get_team_scoreboard():
+    """Return team scoreboard"""
+    try:
+        teams = requests.get(f"{letoctf_api}/team").json()['data']['content']
+        out = [{"name": team['name'], "data": []} for team in teams if not team['admin']]
+        teams_id = [team['id'] for team in teams if not team['admin']]
+
+        for i in range(len(teams_id)):
+            team_data = requests.get(f"{letoctf_api}/score/team/{teams_id[i]}/history").json()['data']['items']['content']
+            data = [{'x': i['submitted'], 'y': i['challenge']['weight']} for i in team_data]
+            out[i]["data"] = data
+
+        return out
+    except:
+        # Обработка исключений при получении данных о командах
+        pass
